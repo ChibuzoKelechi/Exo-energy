5bc5d9b59c791e74b697b3c37ee4641b71aed647
KelechiCV <narutokelechi@gmail.com>
2023-10-11 22:06:52 +0100

    Set up React-router

--- xavion/src/App.jsx
+++ xavion/src/App.jsx
@@ -1,11 +1,50 @@
 import './App.css'
 import Home from "./Pages/Home";
+import Guide from './Pages/Guide';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
 
   return (
-    <>
-      <Home/>
-    </>
+    // <>
+    //   <Home/>
+    // </>
+
+    <Router>
+     <section>
+      <nav className='sidenav'>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/Guide'>Guide</Link>
+          </li>
+          {/* <li>
+            <Link></Link>
+          </li>
+          <li>
+            <Link></Link>
+          </li>
+          <li>
+            <Link></Link>
+          </li> */}
+        </ul>
+      </nav>
+
+      <Switch>
+       
+        <Route path='/' exact>
+         <Home/>
+        </Route>
+
+        <Route path='/Guide' exact>
+         <Guide/>
+        </Route>
+
+
+      </Switch>
+     </section>
+    </Router>
   )
 }
 
